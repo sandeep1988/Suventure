92db6d26d816af82a5fbfe3b7dc1a6c700c1b8e2
sandeep1988 <schourey1988@gmail.com>
2017-06-21 00:43:36 +0530

    modi. on production.rb for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,20 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  config.action_mailer.default_url_options = { :host => 'vast-reaches-58151.herokuapp.com' }  
+  config.action_mailer.delivery_method = :smtp  
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8"  
+  config.action_mailer.smtp_settings = {  
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "vast-reaches-58151.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
